a71f6989a86f8397d75f67ff0a487b3785ccdde6
Reval Fahmi <reval.fahmi42@gmail.com>
2022-11-25 10:32:34 +0700

    Load image with gradle in dashboard

--- app/src/main/java/com/example/movtick/home/DashboardFragment.kt
+++ app/src/main/java/com/example/movtick/home/DashboardFragment.kt
@@ -5,30 +5,27 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
 import com.example.movtick.R
+import com.example.movtick.utils.Preferences
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+import java.text.NumberFormat
+import java.util.Locale
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [DashboardFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class DashboardFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+    private lateinit var tvName: TextView
+    private lateinit var tvBalance: TextView
+    private lateinit var ivProfile: ImageView
+    private lateinit var rvNowPlaying: RecyclerView
+    private lateinit var rvComingSoon: RecyclerView
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private lateinit var preferences: Preferences
+    private lateinit var mDatabase: DatabaseReference
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -38,23 +35,40 @@ class DashboardFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_dashboard, container, false)
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment DashboardFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            DashboardFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+        tvName = requireView().findViewById(R.id.tv_name)
+        tvBalance = requireView().findViewById(R.id.tv_balance)
+        ivProfile = requireView().findViewById(R.id.iv_profile)
+        rvNowPlaying = requireView().findViewById(R.id.rv_now_playing)
+        rvComingSoon = requireView().findViewById(R.id.rv_coming_soon)
+
+        preferences = Preferences(requireActivity().applicationContext)
+        mDatabase = FirebaseDatabase.getInstance().getReference("Film")
+
+        tvName.setText(preferences.getValue("nama"))
+        if (preferences.getValue("saldo").equals("")){
+            tvBalance.text = ""
+        }else{
+            currencyFormat(preferences.getValue("saldo")!!.toDouble(), tvBalance)
+        }
+
+        Glide.with(this)
+            .load(preferences.getValue("url"))
+            .apply(RequestOptions.circleCropTransform())
+            .into(ivProfile)
+
+//        getData()
+    }
+
+    private fun getData(){
+
+    }
+
+    private fun currencyFormat(balance: Double, textView: TextView){
+        val localID = Locale("in", "ID")
+        val format = NumberFormat.getInstance(localID)
+        textView.setText(format.format(balance))
     }
 }
